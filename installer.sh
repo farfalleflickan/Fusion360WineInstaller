1aa0f3730a66be1ba6bfc305859d40b210aa3be5
pastapojken <pastapojken@users.noreply.github.com>
2019-04-21 20:30:55 +0200

    Fixed wrong script version

--- installer.sh
+++ installer.sh
@@ -10,6 +10,7 @@ fi
 case "${1}" in #switch case for the program's argument
     "1")
         tar -xzf installer.tar.gz;
+        #winetricks vcrun2017 d3d11=disabled win7
         winetricks atmlib gdiplus msxml3 msxml6 vcrun2017 corefonts winhttp d3d11=disabled win7;
         echo -e "\n\n"
         echo "----------------------------------------------------------"
@@ -17,25 +18,31 @@ case "${1}" in #switch case for the program's argument
         echo "Watch "$WINEPREFIX"/drive_c/users/"$USER"/Local Settings/Application Data/Autodesk/webdeploy/"
         echo "And wait until a \"production\" folder appears with 2 populated subfolders"
         echo "----------------------------------------------------------"
-        #env WINEPREFIX=$WINEPREFIX wine ./installer/streamer.exe > $WINEPREFIX/streamerLog.txt 2>&1
         export WINEPREFIX=$(pwd);
         wine ./installer/streamer.exe
-        ;;        
+        ;;
+        
     "2")
         echo "Remove the \"d3d11\" entry under libraries"
         export WINEPREFIX=$(pwd);
         winecfg;
+        wine $WINEPREFIX"/drive_c/users/"$USER"/Local Settings/Application Data/Autodesk/webdeploy/production/6a0c9611291d45bb9226980209917c3d/FusionLauncher.exe";
+        ;;
+    "3")
         if [ ! -d dxvkInstaller ]; then
             curl -s https://api.github.com/repos/doitsujin/dxvk/releases/latest | grep ".tar.gz" | cut -d : -f 2,3 | tr -d \" | wget -qi -
             tar -xvf dxvk-*.tar.gz
             rm dxvk-*.tar.gz
             mv dxvk-* dxvkInstaller
             dxvkInstaller/setup_dxvk.sh install
-            #rm -rf dxvkInstaller
-        fi
-        echo "DXVK Installed!";
+            echo "DXVK installed!";
+        else 
+            dxvkInstaller/setup_dxvk.sh uninstall
+            rm -rf dxvkInstaller
+            echo "DXVK uninstalled!";
+        fi     
         ;;
-    "3")
+    "4")
         export WINEPREFIX=$(pwd);
         wine $WINEPREFIX"/drive_c/users/"$USER"/Local Settings/Application Data/Autodesk/webdeploy/production/6a0c9611291d45bb9226980209917c3d/FusionLauncher.exe";
         ;;
