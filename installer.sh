17a86ff6a70736e838ae40bb9d8f7170d9a7879c
pastapojken <pastapojken@users.noreply.github.com>
2019-04-25 22:05:44 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ fi
 case "${1}" in #switch case for the program's argument
     "1")
         tar -xzf installer.tar.gz;
-        winetricks vcrun2017 win7;
+        winetricks corefonts winhttp wininet vcrun2017 win7;
         
         echo -e "\n\n"
         echo "----------------------------------------------------------"
